4ab47c28ec403a67075d6cea72849e5cf8d612ce
Paul Portier <paulportier@gmail.com>
2023-01-14 12:15:44 +0100

    Rescue any errors in the main script

--- kitt_bar.3s.rb
+++ kitt_bar.3s.rb
@@ -7,21 +7,45 @@
 # <xbar.desc>Kitt Plugin</xbar.desc>
 # <xbar.image>https://kitt.lewagon.com/slack/slack-bot-logo.png</xbar.image>
 # <xbar.dependencies>Ruby</xbar.dependencies>
+# require 'bundler/inline'
 
-require 'sqlite3'
-require 'fileutils'
-require 'open3'
-require 'json'
+# gemfile do
+#   source 'https://rubygems.org'
+#   gem 'pry-byebug'
+#   gem 'sqlite3'
+# end
 
-require_relative 'kitt_bar_app/config/setup'
+begin
+  require 'sqlite3'
+  require 'fileutils'
+  require 'open3'
+  require 'json'
 
-batches_serialized = File.read('kitt_bar_app/config/batches.json')
-batches            = JSON.parse(batches_serialized)
 
-CURRENT_BATCHES    = batches.dig('current_batches').map{|batch| batch.transform_keys(&:to_sym)}
-OLD_BATCHES        = batches.dig('old_batches').map{|batch| batch.transform_keys(&:to_sym)}
-KITT_COOKIE        = SessionCookie.firefox
+  require_relative 'kitt_bar_app/config/setup'
 
-require_relative 'kitt_bar_app/plugin'
+  batches_json_file = File.join(__dir__, "kitt_bar_app/config/batches.json")
+  batches_serialized = File.read(batches_json_file)
+  batches            = JSON.parse(batches_serialized)
 
-Plugin.run(CURRENT_BATCHES, OLD_BATCHES)
+  CURRENT_BATCHES    = batches.dig('current_batches').map{|batch| batch.transform_keys(&:to_sym)}
+  OLD_BATCHES        = batches.dig('old_batches').map{|batch| batch.transform_keys(&:to_sym)}
+  KITT_COOKIE        = SessionCookie.firefox
+
+  require_relative 'kitt_bar_app/plugin'
+
+  Plugin.run(CURRENT_BATCHES, OLD_BATCHES)
+rescue => error
+  # binding.pry
+  puts "KittBar Error"
+  puts "---"
+  puts "Error:"
+  puts "---"
+  puts error.full_message
+  puts "---"
+  puts error.message
+  puts error.backtrace
+  puts "---"
+  puts "Please fill the batches.json file with your batches :)"
+  puts "here should be some documentation about how to do so|href=https://github.com/ApaeP/kitt_bar#2---setup-your-batches"
+end
