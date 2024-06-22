c06c6e662d32ca80af8dbeaadc4599c93a4d52e9
Paul Portier <paulportier@gmail.com>
2024-06-22 12:10:41 +0200

    run rubocop

--- kitt_bar_app/config/http_kitt.rb
+++ kitt_bar_app/config/http_kitt.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class HttpKitt
   def initialize
     @kitt_cookie = KITT_COOKIE
-    @base_url = "https://kitt.lewagon.com"
+    @base_url = 'https://kitt.lewagon.com'
   end
 
   class << self
@@ -16,40 +18,32 @@ class HttpKitt
     def patch(camp_slug, action)
       new.patch(camp_slug, action)
     end
-
-    def url_for(params = {})
-      new.url_for(params.ticket, params.camp_slug, params.action)
-    end
   end
 
   def put(ticket, action)
-    "\"#{__dir__}/kitt_request\" param1=PUT param2=#{@kitt_cookie} param3=#{url_for({ticket: ticket, action: action})}"
+    "\"#{__dir__}/kitt_request\" param1=PUT param2=#{@kitt_cookie} param3=#{url_for({ ticket: ticket,
+                                                                                      action: action })}"
   end
 
   def post(camp_slug, action)
-    "\"#{__dir__}/kitt_request\" param1=POST param2=#{@kitt_cookie} param3=#{url_for({camp_slug: camp_slug, action: action})}"
+    "\"#{__dir__}/kitt_request\" param1=POST param2=#{@kitt_cookie} param3=#{url_for({ camp_slug: camp_slug,
+                                                                                       action: action })}"
   end
 
   def patch(camp_slug, action)
-    "\"#{__dir__}/kitt_request\" param1=PATCH param2=#{@kitt_cookie} param3=#{url_for({camp_slug: camp_slug, action: action})}"
+    "\"#{__dir__}/kitt_request\" param1=PATCH param2=#{@kitt_cookie} param3=#{url_for({ camp_slug: camp_slug,
+                                                                                        action: action })}"
   end
 
   def url_for(params = {})
     path = case params[:action]
-    when "done"
-      params[:ticket].mark_as_solved_api_v1_ticket_path
-    when "take"
-      params[:ticket].take_api_v1_ticket_path
-    when "cancel"
-      params[:ticket].cancel_api_v1_ticket_path
-    when "leave"
-      params[:ticket].leave_api_v1_ticket_path
-    when "on_duties"
-      "/api/v1/camps/#{params[:camp_slug]}/on_duties"
-    when "on_duties_projects"
-      "/api/v1/camps/#{params[:camp_slug]}/on_duties?skill_ids=#{SKILLS}"
-    when "finish"
-      "/api/v1/camps/#{params[:camp_slug]}/on_duties/finish"
+    when "done" then params[:ticket].mark_as_solved_api_v1_ticket_path
+    when "take" then params[:ticket].take_api_v1_ticket_path
+    when "cancel" then params[:ticket].cancel_api_v1_ticket_path
+    when "leave" then params[:ticket].leave_api_v1_ticket_path
+    when "on_duties" then "/api/v1/camps/#{params[:camp_slug]}/on_duties"
+    when "on_duties_projects" then "/api/v1/camps/#{params[:camp_slug]}/on_duties?skill_ids=#{SKILLS}"
+    when "finish" then "/api/v1/camps/#{params[:camp_slug]}/on_duties/finish"
     else
       params[:ticket].api_v1_ticket_path
     end
--- kitt_bar_app/ticket.rb
+++ kitt_bar_app/ticket.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Ticket
   attr_reader :content,
               :table,
@@ -16,17 +18,17 @@ class Ticket
               :take_api_v1_ticket_path
 
   def initialize(attr = {})
-    @view = attr.dig(:view)
-    @content = attr.dig(:content)
+    @view = attr[:view]
+    @content = attr[:content]
 
-    @table = attr.dig(:table)
+    @table = attr[:table]
     @student = attr.dig(:user, :name)
-    @teacher =  attr.dig(:assigned, :name)
-    @is_mine = attr.dig(:is_mine)
-    @remote = attr.dig(:remote)
-    @exercice_name = attr.dig(:exercice_name)
-    @owner_slack_uid = attr.dig(:owner_slack_uid)
-    @slack_team_id = attr.dig(:slack_team_id)
+    @teacher = attr.dig(:assigned, :name)
+    @is_mine = attr[:is_mine]
+    @remote = attr[:remote]
+    @exercice_name = attr[:exercice_name]
+    @owner_slack_uid = attr[:owner_slack_uid]
+    @slack_team_id = attr[:slack_team_id]
     @current_user_can_take = attr.dig(:policy, :current_user_can_take)
     @current_user_can_leave = attr.dig(:policy, :current_user_can_leave)
     @current_user_can_update = attr.dig(:policy, :current_user_can_update)
@@ -44,17 +46,17 @@ class Ticket
   end
 
   def assigned_teacher
-    return unless self.teacher
+    return unless teacher
 
-    "x #{self.teacher}"
+    "x #{teacher}"
   end
 
   def header
-    "#{'REMOTE - ' if self.is_remote?}#{self.table ? "table #{self.table}" : 'no table'}"
+    "#{'REMOTE - ' if is_remote?}#{table ? "table #{table}" : 'no table'}"
   end
 
   def plugin_header
-    "#{self.student} @ #{self.table ? "table #{self.table}" : 'no table'}"
+    "#{student} @ #{table ? "table #{table}" : 'no table'}"
   end
 
   def is_remote?
@@ -62,32 +64,31 @@ class Ticket
   end
 
   def is_mine?
-    FULL_NAME == @teacher
+    @teacher == FULL_NAME
   end
 
   def sanitize
-    self.content.gsub(/[\n\r"#:{}|]/, "")
+    content.gsub(/[\n\r"#:{}|]/, '')
   end
 
   def content_formalized
-    ticket_content = self.sanitize.split('').each_slice(40).to_a
+    ticket_content = sanitize.split('').each_slice(40).to_a
     ticket_content.each_with_index do |slice, index|
       loop do
-        break if  (slice.last == " " && slice.length <= 40) || # break if line is perfect
-                  (!slice.include?(" ")) || # break loop to go to cutting line
-                  (!ticket_content[index + 1]) # break if last line of message
+        break if  (slice.last == ' ' && slice.length <= 40) ||
+                  !slice.include?(' ') ||
+                  !ticket_content[index + 1]
+
         ticket_content[index + 1].unshift(slice.pop)
       end
-      # when line is still too long with no space :
-      # 1. keep first part 2. give the rest to next line
-      if slice.length > 40
-        slice = ticket_content[index]
-        ticket_content[index] = slice.each_slice(40).to_a[0] # keep first part
-        if ticket_content[index + 1]
-          ticket_content[index + 1].unshift(slice.each_slice(40).to_a.pop[0]) # give the rest to next line
-        else
-          slice.each_slice(40).to_a.each {|new_slice| ticket_content << new_slice }
-        end
+      next unless slice.length > 40
+
+      slice = ticket_content[index]
+      ticket_content[index] = slice.each_slice(40).to_a[0]
+      if ticket_content[index + 1]
+        ticket_content[index + 1].unshift(slice.each_slice(40).to_a.pop[0])
+      else
+        slice.each_slice(40).to_a.each { |new_slice| ticket_content << new_slice }
       end
     end
     ticket_content.map(&:join).map(&:strip)
